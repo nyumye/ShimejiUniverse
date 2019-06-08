5d3fc8a15222a76da524a5a614b475bd5b9b3fa2
nyumye <myfd642@gmail.com>
2019-06-08 20:41:02 +0900

    クリックした際にのみ仮アニメーションがつくようになった

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -6,13 +6,72 @@ var imgSrc = chrome.extension.getURL(
     ].join('/')
 );
 
+function playPoyo(img, imgSize){
+    $(img).animate(
+        {
+            height: '-=3%'
+        },
+        {
+            duration: "fast", easing: "linear",
+        }
+    ).animate(
+        {
+            height: '+=6%'
+        },
+        {
+            duration: "fast", easing: "linear",
+        }
+    ).animate(
+        {
+            height: '-=3%'
+        },
+        {
+            duration: "fast", easing: "linear",
+        }
+    );
+}
 
 
 $(document).ready(function(){
-    for (var i = 0; i < 20; i++) {
+    //for (var i = 0; i < 20; i++) {
         var img = $('<img>').attr('src', imgSrc).attr('id', 'shimejiSetter');
+        //画像の読み込みが終わった後にちゃんと実行する
+        img.bind("load", function(){
+            let hight = img.hight;
+            let width = img.width;
+
+            //画像のサイズがちゃんと読み込まれているかのテスト
+            console.log(hight,width);
+
+        });
         $('body').append(img);
+        img.css('height', '10%');
+        console.log(img.naturalHeight);
         img.css('left', Math.floor(Math.random(200)*900));
-    }
+        img.click(function(){
+            $(this).animate(
+                {
+                    height: '-=3%'
+                },
+                {
+                    duration: "fast", easing: "linear",
+                }
+            ).animate(
+                {
+                    height: '+=6%'
+                },
+                {
+                    duration: "fast", easing: "linear",
+                }
+            ).animate(
+                {
+                    height: '-=3%'
+                },
+                {
+                    duration: "fast", easing: "linear",
+                }
+            );
+        });
+    //}
     
 });
